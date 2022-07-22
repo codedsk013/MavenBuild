1f1c09f4f74bc2b74ee83939188e97833d7f24e8
Hamza  Sultani <Hamza  Sultani@DESKTOP-S75SKH7>
2022-07-22 16:49:46 +0500

    First Commit

---
+++ WebPortol/src/test/java/Ui/loginTest.java
@@ -0,0 +1,22 @@
+package Ui;
+
+import org.testng.annotations.Test;
+
+public class loginTest {
+
+	@Test
+	
+	public void loginTest() 
+	
+	 {
+		System.out.println("login is successful");
+
+	}
+	
+	@Test
+	public void logoutTest()
+	{
+		System.out.println("logout is successful");
+	}
+
+}
